a41ae5ba4016b1759aa7153f51b8a98a45400d5e
Siva <73848686+SivaPaoren@users.noreply.github.com>
2025-10-02 09:19:33 +0700

    final initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/app/stock',
   experimental: {
     instrumentationHook: true,
   },
-
 };
 
 export default nextConfig;
